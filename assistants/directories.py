fac8f18bfc3b2cbbbe56a9388c8d7141cdf6f505
fariasgvictor <victor.farias@uniriotec.br>
2018-10-02 23:02:23 -0300

    Adding assistant class that is going to be in charge of directories structure for gexf files to be created in

--- assistants/directories.py
+++ assistants/directories.py
@@ -1,2 +1,18 @@
+import os.path
 
+class directories:
 
+    def verify_directory_existence(self, directory_path):
+        return os.path.isdir(directory_path)
+
+    def create_directory_structure(self, directory_path):
+        gexf_directory = os.path.join(directory_path, "gexf")
+        os.mkdir(gexf_directory)
+        ecosystem_gexf_directory = os.path.join(gexf_directory, "ecosystem_gexf")
+        os.mkdir(ecosystem_gexf_directory)
+        projects_gexf_directory = os.path.join(gexf_directory, "projects_gexfs")
+        os.mkdir(projects_gexf_directory)
+
+    def remove_directory_structure(self, directory_path):
+        gexf_directory = os.path.join(directory_path, "gext")
+        os.rmdir(gexf_directory)
