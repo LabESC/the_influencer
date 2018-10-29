00f4257c763df319e103bbd074dd6f8acf571b75
fariasgvictor <victor.farias@uniriotec.br>
2018-10-28 23:13:06 -0300

    Adding the results page

--- pages/app_pages.py
+++ pages/app_pages.py
@@ -14,13 +14,13 @@ class pages(tk.Frame):
 class home_page(pages):
     def __init__(self, *args, **kwargs):
         pages.__init__(self, *args, **kwargs)
-        top_frame = tk.Frame()
+        top_frame = tk.Frame(self)
 
         top_frame.pack()
-        center_frame = tk.Frame()
+        center_frame = tk.Frame(self)
         center_frame.config(pady=50)
         center_frame.pack()
-        bottom_frame = tk.Frame()
+        bottom_frame = tk.Frame(self)
         bottom_frame.pack(side="bottom")
         title_label = tk.Label(top_frame, text="The Influencer")
         title_label.config(font=("Times New Roman", 44))
@@ -60,17 +60,49 @@ class home_page(pages):
 class results_page(pages):
     def __init__(self, *args, **kwargs):
         pages.__init__(self, *args, **kwargs)
+        top_frame = tk.Frame()
+        top_frame.pack()
+
+        title_label = tk.Label(top_frame, text="The Influencer")
+        title_label.config(font=("Times New Roman", 44))
+        title_label.pack()
+
+        center_frame = tk.Frame()
+        center_frame.config(pady=20)
+        center_frame.pack()
+
+        text_message = tk.Label(center_frame, text="Your analysis is done!")
+        text_message.pack()
+
+        show_ecosystem_button = tk.Button(center_frame, text="Show my Ecosystem's Influencer!")
+        show_ecosystem_button.pack()
+
+        bottom_frame = tk.Frame()
+        bottom_frame.config(pady=50)
+        bottom_frame.pack()
+
+        suggestion_for_project = tk.Label(bottom_frame, text="Or you could see the influencer for a specific project...")
+        suggestion_for_project.pack()
+
+        choice = tk.StringVar(self)
+        choices = {"project1", "project2", "project3"}
+        choice.set("project1")
+        ecosystem_options = tk.OptionMenu(bottom_frame, choice, *choices)
+        ecosystem_options.pack()
+
+        show_project_button = tk.Button(bottom_frame, text="Show my Project's Influencer!")
+        show_project_button.pack()
 
 class main_page(tk.Frame):
     def __init__(self, *args, **kwargs):
         tk.Frame.__init__(self, *args, **kwargs)
-        home_page_frame = home_page(self)
-        results_page_frame = results_page(self)
+        self.home_page_frame = home_page(self)
+        self.results_page_frame = results_page(self)
 
         container = tk.Frame(self)
         container.pack(side="top", fill="both", expand=True)
 
-        home_page_frame.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
-        results_page_frame.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
+        self.home_page_frame.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
+        self.results_page_frame.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
 
-        home_page_frame.show()
+        self.results_page_frame.show()
